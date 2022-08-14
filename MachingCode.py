5c555a12a11eb52ccd22d98d9d098718bf0673d0
Throngjwk <105552423+Throngjwk@users.noreply.github.com>
2022-08-14 12:05:05 -0300

    cool bug

---
+++ MachingCode.py
@@ -0,0 +1,18 @@
+linesOfCode = 1
+# Add The Blank String Update Text for Area.
+lineItem = [""]
+# One Item To Be Add Line of Be Got!
+killLineItem = [1]
+
+# Start the Machig Code
+print("Welcome Line 2420 of Maching Code!")
+print("Stepwise Support Types:item")
+print("Type:")
+typeInput = input()
+
+if typeInput == "item":
+    print("Item:")
+    itemInput = input()
+
+if itemInput == "0" | itemInput == "1" | itemInput == "2" | itemInput == "3" | itemInput == "4" | itemInput == "5" | itemInput == "6" | itemInput == "7" | itemInput == "8" | itemInput == "9" | itemInput == "10" | itemInput == "11" | itemInput == "12" | itemInput == "13" | itemInput == "14" | itemInput == "15" | itemInput == "16" | itemInput == "17" | itemInput == "18" | itemInput == "19" | itemInput == "20" | itemInput == "21" | itemInput == "22" | itemInput == "23" | itemInput == "24" | itemInput == "25" | itemInput == "26" | itemInput == "27" | itemInput == "28" | itemInput == "29" | itemInput == "30" | itemInput == "31" | itemInput == "32" | itemInput == "33" | itemInput == "34" | itemInput == "35"  | itemInput == "36":
+    print("|", killLineItem[int(itemInput)], "|: ", lineItem[int(itemInput)])
